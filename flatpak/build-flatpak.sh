25c7b3dea73ed620908ad5b90356e83fa641117b
Markus Mayer <widemeadows@gmail.com>
2023-09-09 22:31:11 +0200

    Add steps to build flatpak into local repo

--- flatpak/build-flatpak.sh
+++ flatpak/build-flatpak.sh
@@ -8,5 +8,10 @@ python3 flatpak-cargo-generator.py ../Cargo.lock -o cargo-sources.json
 echo "Test-installing the FlatPak"
 flatpak-builder --install --force-clean --user build com.github.sunsided.k8sfwd.yaml
 
+# echo "Test-building the FlatPak into a local repo"
+# flatpak-builder --repo=repo --force-clean build com.github.sunsided.k8sfwd.yaml
+# flatpak --user remote-add --no-gpg-verify k8sfwd-repo repo
+# flatpak --user install k8sfwd-repo com.github.sunsided.k8sfwd
+
 echo "Test with"
 echo "flatpak run com.github.sunsided.k8sfwd"
